573cf139a87db446cfb4831627845cae291b0cd4
Oxana <oxanacode@gmail.com>
2022-11-12 14:52:14 +0300

    refactor: replace signIn signUp buttons with authButton

--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -1,11 +1,20 @@
-import { SignInButton } from '../SignInButton';
-import { SignUpButton } from '../SignUpButton';
+import Box from '@mui/joy/Box';
+import { useTranslation } from 'react-i18next';
+
+import { ROUTES } from '../../../constants/routes';
+import { AuthButton } from '../AuthButton';
 
 export const Nav = ({ placedInHeader }: { placedInHeader: boolean }) => {
+  const { t } = useTranslation();
+
+  const handleDisplay = () => {
+    return placedInHeader ? { xs: 'none', sm: 'flex' } : 'flex';
+  };
+
   return (
-    <>
-      <SignInButton placedInHeader={placedInHeader} />
-      <SignUpButton placedInHeader={placedInHeader} />
-    </>
+    <Box component="nav" sx={{ gap: 2, flexDirection: { xs: 'column', sm: 'row' }, display: handleDisplay() }}>
+      <AuthButton route={ROUTES.SIGN_IN.path} variant={'outlined'} text={t('signIn')} />
+      <AuthButton route={ROUTES.SIGN_UP.path} variant={'solid'} text={t('signUp')} />
+    </Box>
   );
 };
