c7210e4c390fb8ed5016ed1e2361f6a414d3c2e7
Oxana <oxanacode@gmail.com>
2022-11-14 04:09:10 +0300

    feat: add 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+import { useTranslation } from 'react-i18next';
+
+import image from '../../assets/images/not-found.svg';
+import { NotFoundButton } from '../../components/NotFoundButton';
 
 export const NotFound = () => {
-  return <div>NotFound Page</div>;
+  const { t } = useTranslation();
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 6,
+        px: 2,
+        py: 6,
+        my: 8,
+        mx: 'auto',
+      }}
+    >
+      <Box sx={{ display: 'flex' }}>
+        <Box component="img" src={image} alt="Not found page" sx={{ width: '100%', maxHeight: 400 }} />
+      </Box>
+      <Typography component="h2" level="h4">
+        {t('pageNotFound')}
+      </Typography>
+      <NotFoundButton />
+    </Box>
+  );
 };
