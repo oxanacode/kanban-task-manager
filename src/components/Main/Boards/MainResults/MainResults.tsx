912cba5c151b18a09de8ce488cd14c24550ef7fb
Igor Asessorov <71071594+Bumble-sakh@users.noreply.github.com>
2022-12-02 20:26:51 +1100

    refactor: no boards (#95)

--- src/components/Main/Boards/MainResults/MainResults.tsx
+++ src/components/Main/Boards/MainResults/MainResults.tsx
@@ -1,6 +1,8 @@
+import { Typography } from '@mui/joy';
 import Box from '@mui/joy/Box';
 import CircularProgress from '@mui/joy/CircularProgress';
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
 import { useGetBoardsByUserIdQuery } from '../../../../store/slices/boards/boardsApi';
@@ -13,6 +15,7 @@ export const MainResults = () => {
   const { isOpenedDialogEditBoard } = useAppSelector((state) => state.boards);
   const { id: userId } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
+  const { t } = useTranslation();
 
   const { data: boards, isLoading, isSuccess } = useGetBoardsByUserIdQuery(userId);
 
@@ -22,7 +25,14 @@ export const MainResults = () => {
     }
   });
 
-  const cards = boards?.map((board) => <BoardCard key={board._id} board={board} />);
+  const cards =
+    boards && boards.length > 0 ? (
+      boards.map((board) => <BoardCard key={board._id} board={board} />)
+    ) : (
+      <Typography level="h2" fontSize="lg">
+        {t('noBoards')}
+      </Typography>
+    );
 
   return (
     <>
