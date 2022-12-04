27591317268a2e97ba8397c40629d2357a4fd378
Oxana <oxanacode@gmail.com>
2022-12-04 12:49:01 +0300

    refactor: adjust form style

--- src/components/Profile/UserInfo/DialogEditProfile/DialogEditProfile.tsx
+++ src/components/Profile/UserInfo/DialogEditProfile/DialogEditProfile.tsx
@@ -99,7 +99,7 @@ export const DialogEditProfile = ({ openDialog, isDialogOpen }: IProps) => {
                 },
                 pattern: {
                   value: /^[a-zA-Zа-яА-Я]{2,10}$/,
-                  message: `${t('wrongFormat')} (${t('twoToTenLetters')})`,
+                  message: t('twoToTenLetters'),
                 },
               }}
               render={({ field }) => (
@@ -112,12 +112,13 @@ export const DialogEditProfile = ({ openDialog, isDialogOpen }: IProps) => {
                   autoComplete="off"
                   title={t('twoToTenLetters')}
                   startDecorator={<AccessibilityNewRoundedIcon />}
+                  sx={{ mb: errors.name ? 0 : 2.75 }}
                 />
               )}
             />
             {errors.name && (
-              <Typography level="body2" color="danger">
-                {errors.name.message}
+              <Typography level="body2" color="danger" sx={{ height: 22 }}>
+                {t('twoToTenLetters')}
               </Typography>
             )}
 
@@ -132,7 +133,7 @@ export const DialogEditProfile = ({ openDialog, isDialogOpen }: IProps) => {
                 },
                 pattern: {
                   value: /^[a-zA-Z0-9]{2,10}$/,
-                  message: `${t('wrongFormat')} (${t('twoToTenLettersLogin')})`,
+                  message: t('twoToTenLettersLogin'),
                 },
               }}
               render={({ field }) => (
@@ -144,23 +145,23 @@ export const DialogEditProfile = ({ openDialog, isDialogOpen }: IProps) => {
                   autoComplete="off"
                   title={t('twoToTenLettersLogin')}
                   placeholder={t('login')}
-                  sx={{ mt: 2 }}
+                  sx={{ mb: errors.login || updateError ? 0.25 : 3 }}
                   startDecorator={<PersonRoundedIcon />}
                 />
               )}
             />
             {errors.login && (
-              <Typography level="body2" color="danger">
-                {errors.login.message}
+              <Typography level="body2" color="danger" sx={{ height: 22 }}>
+                {t('twoToTenLettersLogin')}
               </Typography>
             )}
             {updateError && (
-              <Typography level="body2" color="danger">
+              <Typography level="body2" color="danger" sx={{ height: 22 }}>
                 {t('loginAlreadyExist')}
               </Typography>
             )}
 
-            <Divider sx={{ my: 3 }} />
+            <Divider sx={{ mb: 3 }} />
 
             <Controller
               name="password"
@@ -180,17 +181,18 @@ export const DialogEditProfile = ({ openDialog, isDialogOpen }: IProps) => {
                   autoComplete="off"
                   placeholder={t('password')}
                   label={t('confirmByPassword')}
+                  sx={{ mb: errors.password || logInError ? 0 : 2.75 }}
                   startDecorator={<KeyRoundedIcon />}
                 />
               )}
             />
             {errors.password && (
-              <Typography level="body2" color="danger">
-                {errors.password.message}
+              <Typography level="body2" color="danger" sx={{ height: 22 }}>
+                {t('fieldIsRequire')}
               </Typography>
             )}
             {logInError && (
-              <Typography level="body2" color="danger">
+              <Typography level="body2" color="danger" sx={{ height: 22 }}>
                 {t('wrongPassword')}
               </Typography>
             )}
