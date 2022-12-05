475bd79d7d9fc7d0fd29d56ca429fbdb7c3ee50e
Denna <lilithprimary@gmail.com>
2022-12-05 10:10:58 +0100

    fix: change login to userId for avatar

--- src/utils/getFormData.ts
+++ src/utils/getFormData.ts
@@ -1,11 +1,11 @@
 import { FileType } from '../store/slices/files/filesApi';
 
-export const getFormData = (login: string, file: File, prevFileInfo: FileType | null = null) => {
+export const getFormData = (id: string, file: File, prevFileInfo: FileType | null = null) => {
   const formData = new FormData();
   const newName = prevFileInfo && file.name === prevFileInfo.name ? `${file.name}(1)` : file.name;
 
-  formData.append('boardId', login);
-  formData.append('taskId', login);
+  formData.append('boardId', id);
+  formData.append('taskId', id);
   formData.append('file', file, newName);
   return formData;
 };
