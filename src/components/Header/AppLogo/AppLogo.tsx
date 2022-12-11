55164249cf582a80d0c100b291498b08a09694ce
Oxana <oxanacode@gmail.com>
2022-12-11 23:54:30 +0300

    refactor: add hover to header logo

--- src/components/Header/AppLogo/AppLogo.tsx
+++ src/components/Header/AppLogo/AppLogo.tsx
@@ -1,6 +1,8 @@
 import Link from '@mui/joy/Link';
 import { Link as RouterLink } from 'react-router-dom';
 
+import styles from './appLogo.module.css';
+
 import { ROUTES } from '../../../constants/routes';
 
 export const AppLogo = () => {
@@ -14,7 +16,7 @@ export const AppLogo = () => {
         xmlns="http://www.w3.org/2000/svg"
         aria-labelledby="app-logo"
       >
-        <rect width="40" height="40" rx="8" fill="var(--joy-palette-primary-solidBg)" />
+        <rect width="40" height="40" rx="8" fill="var(--joy-palette-primary-solidBg)" className={styles.logo} />
         <path
           d="M4 7C4 5.34315 5.34315 4 7 4H9C10.6569 4 12 5.34315 12 7V33C12 34.6569 10.6569 36 9 36H7C5.34315 36 4 34.6569 4 33V7Z"
           fill="white"
